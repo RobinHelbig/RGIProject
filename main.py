3a83932d23e9348b623a580e65899d41a94b00eb
Robin Helbig <helbig67@web.de>
2022-09-28 14:19:13 +0100

    Init

---
+++ main.py
@@ -0,0 +1,18 @@
+def indexing():
+    print("indexing")
+
+
+def ranking():
+    print("ranking")
+
+
+def visualize():
+    print("visualize")
+
+
+def evaluation():
+    print("evaluation")
+
+
+print("Start")
+
