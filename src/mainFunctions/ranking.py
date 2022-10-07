6efce677cdc58ddccfc858f724d68ed3c4f3b20a
Robin Helbig <helbig67@web.de>
2022-10-07 19:34:20 +0100

    Fix method signatures

--- src/mainFunctions/ranking.py
+++ src/mainFunctions/ranking.py
@@ -24,7 +24,7 @@ def get_tfs(terms: [str], sentence_index: int, inverted_index: {str: IndexEntry}
     return tfs
 
 
-def get_tfidfs(terms: [str], sentence_index: int, inverted_index: {str: IndexEntry}, corpus_inverted_index: {str: IndexEntry}) -> {str: float}:
+def get_tfidfs(terms: [str], sentence_index: int, inverted_index: {str: IndexEntry}, corpus_idfs: {str: float}) -> {str: float}:
     tfidfs: {str: float} = {}
 
     for term_index, term in enumerate(terms, start=0):
@@ -32,7 +32,7 @@ def get_tfidfs(terms: [str], sentence_index: int, inverted_index: {str: IndexEnt
             continue
 
         i = inverted_index[term]
-        ci = corpus_inverted_index[term]
+        ci = corpus_idfs[term]
 
         tf = next(filter(lambda o: o.document_id == sentence_index, i.occurrences)).frequency
         if tf != 0:
@@ -54,19 +54,19 @@ def get_tfidfs(terms: [str], sentence_index: int, inverted_index: {str: IndexEnt
     return tfidfs
 
 
-def rank_sentences(document: Document, corpus_inverted_index: {str: IndexEntry}, rank_option: str, current_summary: [str]) -> [(str, int)]:
+def rank_sentences(document: Document, corpus_idfs: {str: float}, rank_option: str, current_summary: [str]) -> [(str, int)]:
     sentences = document.text_sentence_terms
 
     inverted_index = indexing(sentences)
 
     for sentence_index, sentence in enumerate(sentences, start=0):
         tfs = get_tfs(sentence, sentence_index, inverted_index)
-        
-        tfidfs = get_tfidfs(sentence, sentence_index, inverted_index, corpus_inverted_index)
+
+        tfidfs = get_tfidfs(sentence, sentence_index, inverted_index, corpus_idfs)
 
     #return sentence : score ordered by position in text
 
-def ranking(document: Document, max_sentences: int, max_chars: int, order_ranked: bool, corpus_inverted_document_frequencies: {str: float}, args: {str: any}):
+def ranking(document: Document, max_sentences: int, max_chars: int, order_ranked: bool, corpus_idfs: {str: float}, args: {str: any}):
     rank_option = args["rank_option"] if args["rank_option"] is not None else "tf-idf" #"tf", "tf-idf", "bm25", "RRF"
     preprocessing = args["preprocessing"] if args["preprocessing"] is not None else False #True, False
     mmr = args["mmr"] if args["mmr"] is not None else False #True, False
