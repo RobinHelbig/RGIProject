e156b0910058b993dd13e5a03ac33fe8ea7a6ea0
ist1105122 <karolina.kinga.kowalczyk@tecnico.ulisboa.pt>
2022-10-10 02:56:00 +0200

    add f-beta measure, map avg, standard deviation and chart

--- src/main.py
+++ src/main.py
@@ -3,7 +3,12 @@
 # nltk.download('maxent_ne_chunker')
 # nltk.download('words')
 
+# from src.mainFunctions.evaluation import calcualte_true_pos
+from src.mainFunctions.evaluation import calculate_precision_recall
+from src.mainFunctions.evaluation import calculate_fbeta_measure
 from src.mainFunctions.evaluation import draw_precision_recall_curve
+from src.mainFunctions.evaluation import get_MAP_avg_by_cat_and_standard_deviation
+from src.mainFunctions.evaluation import draw_MAP_chart
 
 from src.data.document import Document
 
@@ -34,9 +39,16 @@ def visualize(documents: [Document]):
 
 def evaluation(documents: [Document]):
     print("evaluation")
+
+    # true_pos = calcualte_true_pos(test_doc)
+    # precision_recall_tuple = calculate_precision_recall(test_doc, true_pos)
+    # calculate_fbeta_measure(precision_recall_tuple[0], precision_recall_tuple[1])
     draw_precision_recall_curve()
+
+    draw_MAP_chart()
 
 
 print("Start")
 test_doc = Document(1, 'bussines', 'some text', 'referenceSummary', 'summary')
+
 evaluation(test_doc)
