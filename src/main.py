67ae21192b91c45795c03e6fe6956064d08106b2
ist1105122 <karolina.kinga.kowalczyk@tecnico.ulisboa.pt>
2022-10-13 02:52:31 +0200

    try to merge evaluation functions with rest o the code - not finished yet

--- src/main.py
+++ src/main.py
@@ -8,14 +8,11 @@
 # nltk.download('wordnet')
 # nltk.download('omw-1.4')
 
-# from src.mainFunctions.evaluation import calcualte_true_pos
-from src.mainFunctions.evaluation import calculate_precision_recall
+from src.mainFunctions.evaluation import calcualte_true_pos_final
+from src.mainFunctions.evaluation import calculate_precision_recall_final
 from src.mainFunctions.evaluation import calculate_fbeta_measure
-from src.mainFunctions.evaluation import draw_precision_recall_curve
-from src.mainFunctions.evaluation import get_MAP_avg_by_cat_and_standard_deviation
-from src.mainFunctions.evaluation import draw_MAP_chart
-
-from data.document import Document
+from src.mainFunctions.evaluation import calculate_precision_recall_tables_and_MAP_param_final
+from src.mainFunctions.evaluation import draw_precision_recall_curve_final
 
 from operator import attrgetter
 from turtle import title
@@ -32,22 +29,39 @@ from src.mainFunctions.ranking import ranking
 
 documents: [Document]
 
-
 """pass every document you want to evaluate (for example just one document or all of a certain category"""
 
 """pass every document you want to evaluate (for example just one document or all of a certain category"""
 
-#def evaluation(documents: [Document]):
+
+# def evaluation(documents: [Document]):
 
 def evaluation(documents: List[Document]):
     print("evaluation")
+    business = []
+    entertainment = []
+    politics = []
+    sport = []
+    tech = []
+    for document in documents:
+        true_pos = calcualte_true_pos_final(documents)
+        precision_recall_tuple = calculate_precision_recall_final(document.referenceSummary, document.summary, true_pos)
+        calculate_fbeta_measure(precision_recall_tuple[0], precision_recall_tuple[1])
+        precision_recall_tuple = calculate_precision_recall_tables_and_MAP_param_final(document.referenceSummary,
+                                                                                       true_pos)
+        # it will draw chart for every document
+        # draw_precision_recall_curve_final(precision_recall_tuple)
+
+        # get_all_category_docs from reference dir and from 'our' dir ???
+
+
 
     # true_pos = calcualte_true_pos(test_doc)
     # precision_recall_tuple = calculate_precision_recall(test_doc, true_pos)
     # calculate_fbeta_measure(precision_recall_tuple[0], precision_recall_tuple[1])
-    draw_precision_recall_curve()
+    # draw_precision_recall_curve()
 
-    draw_MAP_chart()
+    # draw_MAP_chart()
 
 
 print("Start")
@@ -55,9 +69,9 @@ print("Start")
 # visualize("VisualizeOutput2.txt", mockData(), 2)
 # visualize("VisualizeOutput3.txt", mockData(), 3)
 
-documents_no_preprocessing = read_files(False)
-documents_preprocessing = read_files(True)
-
+# documents_no_preprocessing = read_files(False)
+# documents_preprocessing = read_files(True)
+#
 order_ranked = True
 text_processing = True
 documents = read_files(text_processing)
@@ -68,14 +82,15 @@ for v in index:
 
 for document in documents:
     document.summary = ranking(document, 7, None, order_ranked, corpus_idfs, {"rank_option": "rrf", "mmr": False})
-    # print(document.id, document.summary)
+#     #print(document.id, document.summary)
+#
+#     document_sentences = document.text_sentences
+#     summary_sentences = document.summary
+#     reference_summary_sentences = document.referenceSummary
 
-    document_sentences = document.text_sentences
-    summary_sentences = document.summary
-    reference_summary_sentences = document.referenceSummary
-    # visualize
-    # evaluate
+# visualize
+# evaluate
 
 
 # test_doc = Document(1, 'bussines', 'some text', 'referenceSummary', 'summary')
-# evaluation(test_doc)
+evaluation(documents)
--- src/mainFunctions/evaluation.py
+++ src/mainFunctions/evaluation.py
@@ -1,13 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from src.helper import helper
-from src.data.document import Document
+from src.data.document_old import Document
 import math
+from typing import List
+import csv
 
 import os
 
 BETA = 0.5
 CATEGORIES = ['business', 'entertainment', 'politics', 'sport', 'tech']
+FBETA_HEADER = ['doc', 'fbeta']
 
 
 # helper
@@ -73,8 +76,10 @@ def draw_MAP_chart():
     fig, ax = plt.subplots()
 
     ax.bar(x=CATEGORIES,
-           height=[business_category[0], entertainment_category[0], politics_category[0], sport_category[0], tech_category[0]],
-           yerr=[business_category[1], entertainment_category[1], politics_category[1], sport_category[1], tech_category[1]],
+           height=[business_category[0], entertainment_category[0], politics_category[0], sport_category[0],
+                   tech_category[0]],
+           yerr=[business_category[1], entertainment_category[1], politics_category[1], sport_category[1],
+                 tech_category[1]],
            capsize=4)
 
     plt.title('MAP - comparision')
@@ -188,14 +193,6 @@ def calculate_precision_recall_tables_and_MAP_param_cat(reference_summary_path, 
     MAP = sum(map_precision) / len(reference_summary)
 
     return recall_table, precision_table, MAP
-
-
-def calculate_fbeta_measure(precision, recall):
-    Fbeta = ((1 + math.pow(BETA, 2)) * precision * recall) / (math.pow(BETA, 2) * precision + recall)
-    print("Fbeta: " + str(Fbeta))
-    return Fbeta
-
-
 def draw_precision_recall_curve():
     test_doc = Document(1, 'bussines', 'some text', 'referenceSummary', 'summary')
     true_pos = calcualte_true_pos(test_doc)
@@ -229,3 +226,135 @@ def draw_precision_recall_curve():
     plt.xlabel('recall')
     plt.ylabel('precision')
     plt.show()
+
+#final
+def calcualte_true_pos_final(documents: List[Document]):
+    # reference_summary = helper.extract_sentences(reference_summary_path)
+    # summary = helper.extract_sentences(summary_path)
+    true_pos = []
+    for document in documents:
+        for t1 in document.referenceSummary:
+            for t2 in document.summary:
+                # remove not same
+                if t1 == t2:
+                    true_pos.append(t2)
+    return true_pos
+
+
+def calculate_precision_recall_final(reference_summary, summary, true_pos):
+    # reference_summary_path = '../BBC News Summary/Summaries/business/001.txt'
+    # reference_summary = helper.extract_sentences(reference_summary_path)
+    # summary_path = '../BBC News Summary/Test Summaries/business/001.txt'
+    # summary = helper.extract_sentences(summary_path)
+
+    TP = len(true_pos)
+    FP = len(summary) - TP
+    FN = len(reference_summary) - TP
+
+    try:
+        precision = TP / (TP + FP)
+    except:
+        precision = 1
+
+    try:
+        recall = TP / (TP + FN)
+    except:
+        recall = 1
+
+    return precision, recall
+
+
+def calculate_fbeta_measure(precision, recall):
+    Fbeta = ((1 + math.pow(BETA, 2)) * precision * recall) / (math.pow(BETA, 2) * precision + recall)
+    print("Fbeta: " + str(Fbeta))
+    return Fbeta
+
+
+def calculate_precision_recall_tables_and_MAP_param_final(reference_summary, true_pos):
+    # reference_summary_path = '../BBC News Summary/Summaries/business/001.txt'
+    # reference_summary = helper.extract_sentences(reference_summary_path)
+    recall_table = [0.0]
+    true_pos_counter = 0
+    recall_denominator = len(reference_summary)
+    map_precision = []
+
+    precision_table = [0.0]
+    precision_denominator = 0
+
+    for t in reference_summary:
+        incremented = False
+        if t in true_pos:
+            true_pos_counter = true_pos_counter + 1
+            incremented = True
+        recall_table.append(true_pos_counter / recall_denominator)
+        precision_denominator = precision_denominator + 1
+        precision_table.append(true_pos_counter / precision_denominator)
+        if incremented:
+            map_precision.append(true_pos_counter / precision_denominator)
+
+    MAP = sum(map_precision) / len(reference_summary)
+
+    return recall_table, precision_table, MAP
+
+def draw_precision_recall_curve_final(precision_recall_tuple):
+    # test_doc = Document(1, 'bussines', 'some text', 'referenceSummary', 'summary')
+    # true_pos = calcualte_true_pos(test_doc)
+    # precision_recall_tuple = calculate_precision_recall_tables_and_MAP_param(test_doc, true_pos)
+    plt.rcParams["figure.figsize"] = [7.50, 3.50]
+    plt.rcParams["figure.autolayout"] = True
+
+    plt.rcParams["figure.figsize"] = [7.50, 3.50]
+    plt.rcParams["figure.autolayout"] = True
+
+    r = np.array(precision_recall_tuple[0])
+    p = precision_recall_tuple[1]
+
+    dup_p = p.copy()
+    i = r.shape[0] - 2
+
+    while i >= 0:
+        if p[i + 1] > p[i]:
+            p[i] = p[i + 1]
+        i = i - 1
+
+    fig, ax = plt.subplots()
+
+    for i in range(r.shape[0] - 1):
+        ax.plot((r[i],
+                 r[i]), (p[i], p[i + 1]), 'k-', label='', color='red')
+        ax.plot((r[i], r[i + 1]), (p[i + 1], p[i + 1]), 'k-', label='', color='red')
+
+    ax.plot(r, dup_p, 'k--', color='blue')
+    plt.title('Precision-recall curve')
+    plt.xlabel('recall')
+    plt.ylabel('precision')
+    plt.show()
+
+def get_MAP_avg_by_cat_and_standard_deviation_final(reference_dir, dir):
+    # business
+
+    reference_category = get_all_category_docs(reference_dir)
+    category = get_all_category_docs(dir)
+
+    true_pos = []
+
+    for i in range(len(reference_category)):
+        for j in range(len(category)):
+            if i == j:
+                true_pos.append(calcualte_true_pos_cat(reference_category[i], category[j]))
+
+    reference_MAP = []
+    score_deviation_from_mean = []
+
+    for i in range(len(reference_category)):
+        reference_MAP.append(
+            calculate_precision_recall_tables_and_MAP_param_cat(reference_category[i], true_pos[i])[2])
+
+    reference_MAP_avg = sum(reference_MAP) / len(true_pos)
+
+    for i in range(len(reference_MAP)):
+        score_deviation_from_mean.append(pow(reference_MAP[i] - reference_MAP_avg, 2))
+
+    business_standard_deviation = sum(score_deviation_from_mean) / len(reference_MAP)
+
+    return reference_MAP_avg, business_standard_deviation
