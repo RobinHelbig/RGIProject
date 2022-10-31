a11a75692ea2305fbbd154ab8b77a7dbdd5517c5
cisco18 <hubertijacek@wp.pl>
2022-10-31 15:48:53 +0100

    working tf-idf

--- Submission2/main.py
+++ Submission2/main.py
@@ -7,21 +7,22 @@ from mainFunctions.relevance.extraction import feature_extraction_tf_idf
 """
 clusters
 """
-args = {'n_clusters': 5, 'max_df': 0.2, 'criteria': 'mean'}
-
-text_processing = True
-documents = read_files(text_processing)
-cluster_model = clustering(documents, args)
-cluster_data = cluster_model.labels_
-
-relevant_terms = interpret(cluster_data, documents, args)
-evaluation = evaluate(cluster_data, documents, args)
-plot_dendrogram(cluster_model, truncate_mode='level', p=3)
-
+# args = {'n_clusters': 5, 'max_df': 0.2, 'criteria': 'mean'}
+#
+# text_processing = True
+# documents = read_files(text_processing)
+# cluster_model = clustering(documents, args)
+# cluster_data = cluster_model.labels_
+#
+# relevant_terms = interpret(cluster_data, documents, args)
+# evaluation = evaluate(cluster_data, documents, args)
+# plot_dendrogram(cluster_model, truncate_mode='level', p=3)
+#
 
 """ relevance feedback """
+text_processing = True
 documents = read_files(text_processing)
 print(documents[1].text_sentences)
-feature_extraction_tf_idf("Worries about the deficit concerns about China do, however, remain.", d=documents[1], use_idf=True)
+print(feature_extraction_tf_idf(d=documents[1], use_idf=True))
 
 
