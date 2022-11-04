76b36e37a0441836269eda8612d8b95bcde2eaaf
Robin Helbig <helbig67@web.de>
2022-11-04 14:05:46 +0000

    Copy supervised approach functionality to main jupyter

--- Submission2/mainFunctions/relevance/extraction.py
+++ Submission2/mainFunctions/relevance/extraction.py
@@ -1,4 +1,4 @@
-from Submission2.data.document import Document
+from data.document import Document
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
